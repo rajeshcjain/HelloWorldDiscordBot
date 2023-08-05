9c573a1bcf603859193c8bdace53be9ec3e33bb4
Rajesh Jain <rajeshjain@Rajeshs-MacBook-Pro.local>
2023-08-05 23:51:13 +0530

    commit the initial changes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # HelloWorldDiscordBot
+
+This is a program for devloping the understanding of nodejs and how can you write different bots for discord.
+This is a simple program which will let login the bot with the token and then we will be performing different
+actions based on the input given by the user.
---
+++ app.js
@@ -0,0 +1,21 @@
+const { Client, Events, GatewayIntentBits } = require("discord.js");
+const config = require("./config.json");
+
+//Intenets are the evenst which our bot can receieve from the discor.
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(config.token);
