d47ddd7c309989e785c416a74dccfc4e8dfebdc6
Rajesh Jain <rajeshjain@Rajeshs-MacBook-Pro.local>
2023-08-06 14:07:15 +0530

    This is a complete hello world program

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 This is a program for devloping the understanding of nodejs and how can you write different bots for discord.
 This is a simple program which will let login the bot with the token and then we will be performing different
 actions based on the input given by the user.
+
+intents :
+
+There are different classes which are defined under different "GatewayIntentBits".These different classes have different events defined under them.
+
+So Guilds is equal to "server". so this is the top level type which is there and if it is not being defined then non of the events will not come to bot.
+apart from these
+
+GatewayIntentBits.GuildMembers -> This is for getting the information on the server members.if not defined then you will not receive the member related events and information.
+
+GatewayIntentBits.GuildMessages -> This is related with all the messages whicgh are posted in the server.if this is not as a part of the list
+then non of the messages(GatewayIntentBits.DirectMessages,GatewayIntentBits.MessageContent,) will reach to bot.
+
+Partials is still an open question which need to be understand.
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, Partials, Events } = require("discord.js");
 const config = require("./config.json");
 
 //Intenets are the evenst which our bot can receieve from the discor.
@@ -8,13 +8,80 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.Guilds,
-    GatewayIntentBits.DirectMessages,
-    GatewayIntentBits.MessageContent,
+    // GatewayIntentBits.DirectMessages,
+    // GatewayIntentBits.MessageContent,
   ],
+  partials: [Partials.GuildMember],
 });
 
-client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+// const client = new Client({
+//   intents: [
+//     GatewayIntentBits.GuildMembers,
+//     GatewayIntentBits.GuildMessages,
+//     GatewayIntentBits.Guilds,
+//     GatewayIntentBits.DirectMessages,
+//     GatewayIntentBits.MessageContent,
+//   ],
+//   partials: [
+//     Partials.Channel,
+//     Partials.Message,
+//     Partials.User,
+//     Partials.GuildMember,
+//     Partials.GuildScheduledEvent,
+//   ],
+// });
+
+client.once(Events.ClientReady, (client) => {
+  console.log("Ready! Logged in as " + client.user.tag);
+});
+
+client.on(Events.MessageCreate, (msg) => {
+  let msgContent = msg.content.toLowerCase();
+  console.log("1 " + msgContent);
+
+  if (msg.author.bot == true) {
+    console.log("This message is from the bot");
+    return;
+  }
+
+  if (!msg.author.bot) {
+    msg.reply("You are not a bot user");
+  }
+
+  if (msgContent === "!help" || msgContent === "!command") {
+    msg.reply(
+      "This bot operates on the following commands: !help !commands !age !math"
+    );
+  }
+
+  // basic math return the sum of a few numbers using addition, subtraction, multip. etc.
+  if (msgContent === "!math") {
+    msg.reply(
+      "5 + 2 - 1 * 5 / 2 - 4 + 7 * 3 % 5 = " +
+        (5 + 2 - (1 * 5) / 2 - 4 + ((7 * 3) % 5))
+    );
+  }
+
+  if (msgContent === "!age") {
+    msg.reply("The server was started at " + msg.guild.createdAt.toString());
+  }
+
+  // Fetch guild members returns a promise.
+  msg.guild.members.fetch().then(
+    (value) => {
+      // console.log(value);
+      value.forEach((user) => {
+        // print each user's id and the id of the author of the message
+        console.log(user.user.id + " " + msg.author.id);
+        console.log(user.joinedTimestamp); // print joinedTimestamp
+        let date = new Date(user.joinedTimestamp); // Convert milliseconds to actual date and time
+        msg.reply(user.user.tag + " joined\n" + date.toString()); // reply with the date and time
+      });
+    },
+    (error) => {
+      console.log(error); // prints error if it occurs.
+    }
+  );
 });
 
 // Log in to Discord with your client's token
